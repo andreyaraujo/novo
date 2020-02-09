f7d41c24d88586202faa6dc470970a35208ca37b
Andrêy Araújo <andreyaraujored@gmail.com>
2020-02-09 17:19:59 -0300

    Add files via upload

--- create.php
+++ create.php
@@ -4,86 +4,97 @@
 <head>
     <meta charset="utf-8">
     <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-  <title>Adicionar Contato</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <title>Adicionar Contato</title>
 </head>
 
 <body>
     <div class="container">
         <div clas="span10 offset1">
-          <div class="card">
-            <div class="card-header">
-                <h3 class="well"> Adicionar Contato </h3>
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="well"> Adicionar Contato </h3>
+                </div>
+                <div class="card-body">
+                    <form class="form-horizontal" action="create.php" method="post">
+
+                        <div class="control-group <?php echo !empty($nomeErro) ? 'error ' : ''; ?>">
+                            <label class="control-label">Nome</label>
+                            <div class="controls">
+                                <input size="50" class="form-control" name="nome" type="text" placeholder="Nome" required="" value="<?php echo !empty($nome) ? $nome : ''; ?>">
+                                <?php if (!empty($nomeErro)) : ?>
+                                    <span class="help-inline"><?php echo $nomeErro; ?></span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="control-group <?php echo !empty($enderecoErro) ? 'error ' : ''; ?>">
+                            <label class="control-label">Endereço</label>
+                            <div class="controls">
+                                <input size="80" class="form-control" name="endereco" type="text" placeholder="Endereço" required="" value="<?php echo !empty($endereco) ? $endereco : ''; ?>">
+                                <?php if (!empty($emailErro)) : ?>
+                                    <span class="help-inline"><?php echo $enderecoErro; ?></span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="control-group <?php echo !empty($telefoneErro) ? 'error ' : ''; ?>">
+                            <label class="control-label">Telefone</label>
+                            <div class="controls">
+                                <input size="35" class="form-control" name="telefone" type="text" placeholder="Telefone" required="" value="<?php echo !empty($telefone) ? $telefone : ''; ?>">
+                                <?php if (!empty($emailErro)) : ?>
+                                    <span class="help-inline"><?php echo $telefoneErro; ?></span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="control-group <?php echo !empty($emailErro) ? 'error ' : ''; ?>">
+                            <label class="control-label">Email</label>
+                            <div class="controls">
+                                <input size="40" class="form-control" name="email" type="text" placeholder="Email" required="" value="<?php echo !empty($email) ? $email : ''; ?>">
+                                <?php if (!empty($emailErro)) : ?>
+                                    <span class="help-inline"><?php echo $emailErro; ?></span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="control-group <?php echo !empty($sexoErro) ? 'error ' : ''; ?>">
+                            <label class="control-label">Sexo</label>
+                            <div class="controls">
+                                <div class="form-check form-check-inline">
+                                    <p class="form-check-label">
+                                        <input class="form-check-input" type="radio" name="sexo" id="sexo" value="M"> Masculino
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="sexo" id="sexo" value="F"> Feminino
+                                </div>
+                                </p>
+                                <?php if (!empty($sexoErro)) : ?>
+                                    <span class="help-inline"><?php echo $sexoErro; ?></span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="control-group <?php echo !empty($senhaErro) ? 'error ' : ''; ?>">
+                            <label class="control-label">Senha</label>
+                            <div class="controls">
+                                <input size="40" class="form-control" name="senha" type="text" placeholder="Senha" required="" value="<?php echo !empty($senha) ? $senha : ''; ?>">
+                                <?php if (!empty($senhaErro)) : ?>
+                                    <span class="help-inline"><?php echo $senhaErro; ?></span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="form-actions">
+                            <br />
+
+                            <button type="submit" class="btn btn-success">Adicionar</button>
+                            <a href="index.php" type="btn" class="btn btn-default">Voltar</a>
+
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div class="card-body">
-            <form class="form-horizontal" action="create.php" method="post">
-
-                <div class="control-group <?php echo !empty($nomeErro)?'error ' : '';?>">
-                    <label class="control-label">Nome</label>
-                    <div class="controls">
-                        <input size="50" class="form-control" name="nome" type="text" placeholder="Nome" required="" value="<?php echo !empty($nome)?$nome: '';?>">
-                        <?php if(!empty($nomeErro)): ?>
-                            <span class="help-inline"><?php echo $nomeErro;?></span>
-                            <?php endif;?>
-                    </div>
-                </div>
-
-                <div class="control-group <?php echo !empty($enderecoErro)?'error ': '';?>">
-                    <label class="control-label">Endereço</label>
-                    <div class="controls">
-                        <input size="80" class="form-control" name="endereco" type="text" placeholder="Endereço" required="" value="<?php echo !empty($endereco)?$endereco: '';?>">
-                        <?php if(!empty($emailErro)): ?>
-                            <span class="help-inline"><?php echo $enderecoErro;?></span>
-                            <?php endif;?>
-                    </div>
-                </div>
-
-                <div class="control-group <?php echo !empty($telefoneErro)?'error ': '';?>">
-                    <label class="control-label">Telefone</label>
-                    <div class="controls">
-                        <input size="35" class="form-control" name="telefone" type="text" placeholder="Telefone" required="" value="<?php echo !empty($telefone)?$telefone: '';?>">
-                        <?php if(!empty($emailErro)): ?>
-                            <span class="help-inline"><?php echo $telefoneErro;?></span>
-                            <?php endif;?>
-                    </div>
-                </div>
-
-                <div class="control-group <?php echo !empty($emailErro)?'error ': '';?>">
-                    <label class="control-label">Email</label>
-                    <div class="controls">
-                        <input size="40" class="form-control" name="email" type="text" placeholder="Email" required="" value="<?php echo !empty($email)?$email: '';?>">
-                        <?php if(!empty($emailErro)): ?>
-                            <span class="help-inline"><?php echo $emailErro;?></span>
-                            <?php endif;?>
-                    </div>
-                </div>
-
-                <div class="control-group <?php echo !empty($sexoErro)?'error ': '';?>">
-                    <label class="control-label">Sexo</label>
-                    <div class="controls">
-                        <div class="form-check">
-                            <p class="form-check-label">
-                                <input class="form-check-input" type="radio" name="sexo" id="sexo" value="M" <?php echo ($sexo=="M" ) ? "checked" : null; ?>/> Masculino
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="sexo" id="sexo" value="F" <?php echo ($sexo=="F" ) ? "checked" : null; ?>/> Feminino
-                        </div>
-                        </p>
-                        <?php if(!empty($sexoErro)): ?>
-                            <span class="help-inline"><?php echo $sexoErro;?></span>
-                            <?php endif;?>
-                    </div>
-                </div>
-                <div class="form-actions">
-                    <br/>
-
-                    <button type="submit" class="btn btn-success">Adicionar</button>
-                    <a href="index.php" type="btn" class="btn btn-default">Voltar</a>
-
-                </div>
-            </form>
-          </div>
-        </div>
         </div>
     </div>
     </div>
@@ -96,70 +107,68 @@
 </html>
 
 <?php
-    require 'banco.php';
+require 'banco.php';
 
-    if(!empty($_POST))
-    {
-        //Acompanha os erros de validação
-        $nomeErro = null;
-        $enderecoErro = null;
-        $telefoneErro = null;
-        $emailErro = null;
-        $sexoErro = null;
+if (!empty($_POST)) {
+    //Acompanha os erros de validação
+    $nomeErro = null;
+    $enderecoErro = null;
+    $telefoneErro = null;
+    $emailErro = null;
+    $sexoErro = null;
+    $senhaErro = null;
 
-        $nome = $_POST['nome'];
-        $endereco = $_POST['endereco'];
-        $telefone = $_POST['telefone'];
-        $email = $_POST['email'];
-        $sexo = $_POST['sexo'];
+    $nome = $_POST['nome'];
+    $endereco = $_POST['endereco'];
+    $telefone = $_POST['telefone'];
+    $email = $_POST['email'];
+    $sexo = $_POST['sexo'];
+    $senha = $_POST['senha'];
 
-        //Validaçao dos campos:
-        $validacao = true;
-        if(empty($nome))
-        {
-            $nomeErro = 'Por favor digite o seu nome!';
-            $validacao = false;
-        }
-
-        if(empty($endereco))
-        {
-            $enderecoErro = 'Por favor digite o seu endereço!';
-            $validacao = false;
-        }
-
-        if(empty($telefone))
-        {
-            $telefoneErro = 'Por favor digite o número do telefone!';
-            $validacao = false;
-        }
-
-        if(empty($email))
-        {
-            $telefoneErro = 'Por favor digite o endereço de email';
-            $validacao = false;
-        }
-        elseif (!filter_var($email,FILTER_VALIDATE_EMAIL))
-        {
-            $emailError = 'Por favor digite um endereço de email válido!';
-            $validacao = false;
-        }
-
-        if(empty($sexo))
-        {
-            $sexoErro = 'Por favor digite o campo!';
-            $validacao = false;
-        }
-
-        //Inserindo no Banco:
-        if($validacao)
-        {
-            $pdo = Banco::conectar();
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $sql = "INSERT INTO pessoa (nome, endereco, telefone, email, sexo) VALUES(?,?,?,?,?)";
-            $q = $pdo->prepare($sql);
-            $q->execute(array($nome,$endereco,$telefone,$email,$sexo));
-            Banco::desconectar();
-            header("Location: index.php");
-        }
+    //Validaçao dos campos:
+    $validacao = true;
+    if (empty($nome)) {
+        $nomeErro = 'Por favor digite o seu nome!';
+        $validacao = false;
     }
+
+    if (empty($endereco)) {
+        $enderecoErro = 'Por favor digite o seu endereço!';
+        $validacao = false;
+    }
+
+    if (empty($telefone)) {
+        $telefoneErro = 'Por favor digite o número do telefone!';
+        $validacao = false;
+    }
+
+    if (empty($email)) {
+        $telefoneErro = 'Por favor digite o endereço de email';
+        $validacao = false;
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailError = 'Por favor digite um endereço de email válido!';
+        $validacao = false;
+    }
+
+    if (empty($sexo)) {
+        $sexoErro = 'Por favor digite o campo!';
+        $validacao = false;
+    }
+
+    if (empty($senha)) {
+        $senhaErro = 'Por favor digite o campo!';
+        $validacao = false;
+    }
+
+    //Inserindo no Banco:
+    if ($validacao) {
+        $pdo = Banco::conectar();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "INSERT INTO pessoa (nome, endereco, telefone, email, sexo, senha) VALUES(?,?,?,?,?,?)";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($nome, $endereco, $telefone, $email, $sexo, $senha));
+        Banco::desconectar();
+        /* header("Location: index.php"); */
+    }
+}
 ?>
